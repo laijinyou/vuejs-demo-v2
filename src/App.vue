394a21360e3aa6aaf50c4466c2dc19d5943c2713
Sloth <1183820@qq.com>
2020-06-30 08:42:20 +0900

    演示 pug 的使用，后续代码不需要

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Welcome to Vue.js 2.x demo.</h1>
-  </div>
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
 </template>
 
 <script>
@@ -11,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,6 +18,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 200px;
+
+  h1 {
+    color: #42b983;
+  }
 }
 </style>
+<!-- 通过为 <template> 和 <style> 指定 lang 属性，我们就可以使用其他语法来书写代码。-->
 <!-- 以 .vue 结尾的文件构成了一个单文件组件，它可以由 <template>、<script> 和 <style> 三个部分组成，分别对应模板、脚本和样式。-->
