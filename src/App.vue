690b6c02b14057becb458bf024847f0affd4e793
Sloth <1183820@qq.com>
2020-06-30 08:52:50 +0900

    引入bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<template lang="pug">
-  div#app
-    img(src="./assets/logo.png")
-    h1 Welcome from pug & sass
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome from Bootstrap</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank">Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span></a></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,18 +17,12 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 200px;
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
 
-  h1 {
-    color: #42b983;
-  }
-}
+body > .container { margin-top: 15px;}
 </style>
 <!-- 通过为 <template> 和 <style> 指定 lang 属性，我们就可以使用其他语法来书写代码。-->
 <!-- 以 .vue 结尾的文件构成了一个单文件组件，它可以由 <template>、<script> 和 <style> 三个部分组成，分别对应模板、脚本和样式。-->
+<!-- 上面的 <style> 部分使用了 scss 语法，它是 Sass（一种 CSS 扩展语言）语法的一种，Sass 允许我们在 CSS 中书写支持变量等编程特性的语法。$icon-font-path 是 bootstrap-sass 的一个变量，用于指定字体路径，它默认使用了一个相对路径的地址，在使用 @import 引入样式前，我们需要手动将其指向 "~bootstrap-sass/assets/fonts/bootstrap/"，不指定会报找不到字体模块的错误 -->
+<!-- "~bootstrap-sass/assets/fonts/bootstrap/" 中的波浪号（~）会将后面的内容当模块路径对待，默认指向 node_modules 目录。-->
