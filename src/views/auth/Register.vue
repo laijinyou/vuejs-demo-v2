167760d3c49a9818b0bb782b50e0b91113fece88
Sloth <1183820@qq.com>
2020-06-30 10:49:23 +0900

    添加验证码

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -8,28 +8,35 @@
 
         <!-- 我们先在外层元素上，添加 data-validator-form 属性，使其成为所有验证项的父级.-->
         <div class="panel-body" data-validator-form>
+
           <div class="form-group">
             <label class="control-label">用户名</label>
             <input v-validator:input.required="{ regex: /^[a-zA-Z]+\w*\s?\w*$/, error: '用户名要求以字母开头的单词字符' }" type="text" class="form-control" placeholder="请填写用户名">
           </div>
+
           <div class="form-group">
             <label class="control-label">密码</label>
             <input id="password" v-validator.required="{ regex: /^\w{6,16}$/, error: '密码要求 6 ~ 16 个单词字符' }" type="password" class="form-control" placeholder="请填写密码">
           </div>
+
           <div class="form-group">
             <label class="control-label">确认密码</label>
             <input v-validator.required="{ target: '#password' }" type="password" class="form-control" placeholder="请填写确认密码">
           </div>
+
           <div class="form-group">
             <label class="control-label">图片验证码</label>
             <input v-validator.required="{ title: '图片验证码' }" type="text" class="form-control" placeholder="请填写验证码">
           </div>
-          <div class="thumbnail" title="点击图片重新获取验证码">
-            <div class="captcha"></div>
+
+          <div class="thumbnail" title="点击图片重新获取验证码" @click="getCaptcha">
+            <div class="captcha vcenter" v-html="captchaTpl"></div>
           </div>
+
           <button type="submit" class="btn btn-lg btn-success btn-block">
             <i class="fa fa-btn fa-sign-in"></i> 注册
           </button>
+
         </div>
 
       </div>
@@ -38,12 +45,32 @@
 </template>
 
 <script>
+import createCaptcha from '@/utils/createCaptcha'
+
 export default {
-  name: 'Register'
+  name: 'Register',
+  data() {
+    return {
+      captchaTpl: '' // 验证码模板
+    }
+  },
+  // 生命周期钩子 created 在实例创建完成后被立即调用
+  created() {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha() {
+      const { tpl, captcha } = createCaptcha()
+
+      this.captchaTpl = tpl
+      this.localCaptcha = captcha
+    }
+  }
 }
 </script>
 
 <style scoped>
 .thumbnail { width: 170px; margin-top: 10px; cursor: pointer;}
 .thumbnail .captcha { height: 46px; background: #E1E6E8;}
+.captcha { font-size: 24px; font-weight: bold; user-select: none;}
 </style>
