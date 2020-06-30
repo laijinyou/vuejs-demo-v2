93a442406bd06165e8aeac670f0780ed1ef4c172
Sloth <1183820@qq.com>
2020-06-30 18:00:39 +0900

    mock.js假数据

--- src/main.js
+++ src/main.js
@@ -13,12 +13,35 @@ import VueSweetalert2 from './plugins/vue-sweetalert2'
 // 引入插件
 import Message from './plugins/message'
 import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
 
 // 使用插件
 Vue.use(VueSweetalert2)
 Vue.use(Message)
 // 设置 false 以阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false
+
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(10)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
 
 // new Vue 创建一个新的 Vue 实例，然后在参数选项中添加 render 选项 ，
 // 该函数用来生成虚拟节点，最后使用 $mount 方法将其挂在到 #app。
