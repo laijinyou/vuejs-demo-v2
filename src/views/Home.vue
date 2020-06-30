998ded7cf2cfe3a951ad440975fc91973e61e579
Sloth <1183820@qq.com>
2020-06-30 18:41:47 +0900

    过滤帖子列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,9 @@
       <div class="panel panel-default">
         <div class="panel-heading">
           <ul class="list-inline topic-filter">
-            <li><a href="/topics?filter=default" class="active">活跃</a></li>
-            <li><a href="/topics?filter=excellent">精华</a></li>
-            <li><a href="/topics?filter=vote">投票</a></li>
-            <li><a href="/topics?filter=recent">最近</a></li>
-            <li><a href="/topics?filter=noreply">零回复</a></li>
+            <li v-for="item in filters" :key="item">
+              <router-link v-title="item.title" :class="{ active: filter === item.filter }" :to="`/topics?filter=${item.filter}`">{{ item.name }}</router-link>
+            </li>
           </ul>
           <div class="clearfix"></div>
         </div>
@@ -57,55 +55,58 @@ export default {
   name: 'Home',
   data() {
     return {
-      msg: '', // 消息
-      msgType: '', // 消息类型
-      msgShow: false // 是否显示消息，默认不显示
+      msg: '',
+      msgType: '',
+      msgShow: false,
+      articles: [], // 文章列表
+      filter: 'default', // 默认过滤方式
+      filters: [ // 过滤方式列表
+        { filter: 'default', name: '活跃', title: '最后回复排序'},
+        { filter: 'excellent', name: '精华', title: '只看加精的话题'},
+        { filter: 'vote', name: '投票', title: '点赞数排序'},
+        { filter: 'recent', name: '最近', title: '发布时间排序'},
+        { filter: 'noreply', name: '零回复', title: '无人问津的话题'}
+      ],
     }
   },
-  // 是组件内的路由导航守卫，在确认渲染该组件的对应路由前调用。该守卫不能访问 this，但我们通过传一个回调给 next，就可以使用上面的 vm 来访问组件实例。
   beforeRouteEnter(to, from, next) {
-    // 路由的名称，对应路由配置中的 name
     const fromName = from.name
-    // 获取 logout 参数
     const logout = to.params.logout
 
-    // 确认导航
     next(vm => {
-      // 通过 vm 参数访问组件实例，已登录时，评估路由名称
       if (vm.$store.state.auth) {
         switch (fromName) {
-          // 如果从注册页面跳转过来  
           case 'Register':
-            // 显示注册成功  
             vm.showMsg('注册成功')
             break
-            // 已登录时，从登录页面跳转过来
-        case 'Login':
-          // 显示登录成功
-          vm.showMsg('登录成功')
-          break
+          case 'Login':
+            vm.showMsg('登录成功')
+            break
         }
       } else if (logout) {
-        // logout 返回 true 时，显示操作成功提示
         vm.showMsg('操作成功')
       }
+
+      // 确认渲染该组件的对应路由时，设置相关数据
+      vm.setDataByFilter(to.query.filter)
     })
   },
   computed: {
     ...mapState([
       'auth',
       'user'
-    ]),
-    articles() {
-      return this.$store.getters.computedArticles
-    }
+    ])
+    // 这里删除了原来的 articles
   },
   watch: {
-    // 监听 auth，它的值变为 false 时，显示操作成功提示
     auth(value) {
       if (!value) {
         this.showMsg('操作成功')
       }
+    },
+    // 监听 '$route'，在查询参数变化后，设置相关数据
+    '$route'(to) {
+      this.setDataByFilter(to.query.filter)
     }
   },
   methods: {
@@ -113,6 +114,13 @@ export default {
       this.msg = msg
       this.msgType = type
       this.msgShow = true
+    },
+    // 设置相关数据
+    setDataByFilter(filter = 'default') {
+      // 设置当前过滤方式为查询参数的 filter
+      this.filter = filter
+      // 设置文章列表为过滤后的所有文章
+      this.articles = this.$store.getters.getArticlesByFilter(filter)
     }
   }
 }
