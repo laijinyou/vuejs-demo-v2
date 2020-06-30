4a2066a97b5096978f8a51375c4d4360492fce9f
Sloth <1183820@qq.com>
2020-06-30 11:49:51 +0900

    完善登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,37 @@ const routes = [
     path: '/auth/register',
     name: 'Register',
     component: () => import('@/views/auth/Register')
+  },
+  // 首页路由配置
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  // 其他未配置的路由都跳转到首页
+  {
+    path: '*',
+    // 重定向
+    redirect: '/'
   }
 ]
 
-const router = new Router({
+const router =  new Router({
   mode: 'history',
   routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  // 获取仓库里的登录信息，使用 router.app 可以获取 router 对应的 Vue 根实例，使用实例的 $options.store 可以从选项中访问仓库
+  const auth = router.app.$options.store.state.auth
+
+  if (auth && to.path.indexOf('/auth/') !== -1) {
+    // 如果当前用户已登录，且目标路由包含 /auth/ ，就跳转到首页
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
